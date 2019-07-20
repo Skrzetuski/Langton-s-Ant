6de137ab9cc30ab59bd357629ce2b4b97e9c2568
Krystian Gumiński <krystian.guminsk@gmail.com>
2019-07-20 21:11:32 +0200

    Created first version of app

---
+++ src/main/java/application/controllers/Controller.java
@@ -0,0 +1,96 @@
+package application.controllers;
+
+import application.interfaces.CanvasNames;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import static application.controllers.MainController.ant;
+import static application.controllers.MainController.bundle;
+
+
+public class Controller implements Initializable, CanvasNames {
+
+    @FXML
+    Canvas canvas;
+
+    @FXML
+    Button change;
+
+    private GraphicsContext gc;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        canvas.setHeight(HEIGHT);
+        canvas.setWidth(WIDTH);
+
+        gc = canvas.getGraphicsContext2D();
+
+        if (MainController.image == null) {
+            gc.setFill(Color.WHITE);
+            gc.fillRect(START_PIXEL_X, START_PIXEL_Y, HEIGHT, WIDTH);
+        } else {
+            gc.drawImage(MainController.image, START_PIXEL_X, START_PIXEL_Y);
+        }
+    }
+
+    public void switchLang() throws IOException {
+
+        stop();
+        MainController.image = canvas.snapshot(null, null);
+
+        if (change.getText().equals("pl"))
+            bundle = ResourceBundle.getBundle("/bundles/Bundle_pl_PL", new Locale("pl", "PL"));
+        else
+            bundle = ResourceBundle.getBundle("/bundles/Bundle_en_EN", new Locale("en", "EN"));
+
+        canvas.getScene().setRoot(FXMLLoader.load(getClass().getResource("/view/main.fxml"), bundle));
+    }
+
+    public void draw() {
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    public void stop() {
+        timeline.stop();
+    }
+
+    final Timeline timeline = new Timeline(
+            new KeyFrame(Duration.seconds(0.001), actionEvent -> {
+
+                WritableImage snap = canvas.getGraphicsContext2D().getCanvas().snapshot(null, null);
+
+                ant.checkPosition();
+
+                if (snap.getPixelReader().getColor(ant.getPositionX(), ant.getPositionY()).equals(Color.WHITE)) {
+
+                    gc.setFill(Color.BLACK);
+                    gc.fillRect(ant.getPositionX(), ant.getPositionY(), ant.getSize(), ant.getSize());
+                    ant.turnLeft();
+
+                } else {
+
+                    gc.setFill(Color.WHITE);
+                    gc.fillRect(ant.getPositionX(), ant.getPositionY(), ant.getSize(), ant.getSize());
+                    ant.turnRight();
+
+                }
+            })
+    );
+}
---
+++ src/main/java/application/interfaces/CanvasNames.java
@@ -0,0 +1,12 @@
+package application.interfaces;
+
+public interface CanvasNames {
+
+    Integer START_PIXEL_X = 0;
+
+    Integer START_PIXEL_Y = 0;
+
+    Integer WIDTH = 400;
+
+    Integer HEIGHT = 400;
+}
